b13571fc35135a001a3160ff3d9cc9ca5ae52d0b
yzb0612 <957626536@qq.com>
2023-06-19 16:57:50 +0800

    add index and bootstrap

--- src/main/java/com/bbs/bbs/controller/IndexController.java
+++ src/main/java/com/bbs/bbs/controller/IndexController.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
-    }
+public class IndexController {
+    @GetMapping("/")
+    public String index(){ return "index";}
 }
