2068a8ad58dd236b5e0f000e883bdd0a73e1b09b
aibo <957626536@qq.com>
2023-06-19 14:59:21 +0800

    init repo

---
+++ src/main/java/com/bbs/bbs/BbsApplication.java
@@ -0,0 +1,14 @@
+package com.bbs.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+ 
+public class BbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsApplication.class, args);
+    }
+
+}
